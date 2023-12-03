8cc96b640ffc272628bd50dd98a18dd30a915221
Alexandra Astileanu <alexandra.astileanu@gmail.com>
2023-12-03 18:27:21 +0200

    Derive ActivePlayer state from gameTurns

--- src/components/Log.jsx
+++ src/components/Log.jsx
@@ -1,12 +1,11 @@
 export default function Log({ turns }) {
   return (
     <ol id="log">
-      {turns.length > 0 &&
-        turns.map((turn) => (
-          <li key={`${turn.square.row}${turn.square.col}`}>
-            {turn.player} selected ({turn.square.row}, {turn.square.col})
-          </li>
-        ))}
+      {turns.map((turn) => (
+        <li key={`${turn.square.row}${turn.square.col}`}>
+          {turn.player} selected ({turn.square.row}, {turn.square.col})
+        </li>
+      ))}
     </ol>
   );
 }
