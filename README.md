d0ca83644d220eedd55d6cf8d07fc88918152321
Alexandra Astileanu <alexandra.astileanu@gmail.com>
2023-12-03 22:00:48 +0200

    Final refactoring and documentation

--- README.md
+++ README.md
@@ -2,9 +2,27 @@
 
 ## Tic tac toe game
 
-A react demo
+Yet another implementation of the game. Here are the lessons that I've learned during this deeper dive into React:
+- React State: React is scheduling state updates, not performing them instantly. By using the function form to set new state values, we can expect correct values to be set
+- Immutability when updating object State (reference values, deep copy)
+- Two way Binding
+- Lifting State up to the closest ancestor component
+- Overriding a value in a structure at state change
 
-### How to run 
-`npm install` to install project
-`npm run dev` to start the development server
+### How to run the project
+Install the project:
+> `$ npm install` 
+ 
+To start the development server:
+> `$ npm run dev` 
 
+## Project final look
+
+#### Player 1 made the first move, now it's Player 2's turn
+![Player 1 made the first move](./public/img-result.png)
+
+#### Editing Player 1's name
+![Editing Player 1's name](./public/img-edit.png)
+
+#### Player 1 has won
+![Player 1 has won](./public/img-winner.png)
--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,11 @@ import Log from './components/Log';
 import GameOver from './components/GameOver';
 import { WINNING_COMBINATIONS } from '../winning-combinations';
 
-const initialGameBoard = [
+const PLAYERS = {
+  X: 'Player 1',
+  O: 'Player 2',
+};
+const INITIAL_GAME_BOARD = [
   [null, null, null],
   [null, null, null],
   [null, null, null],
@@ -22,26 +26,9 @@ function deriveActivePlayer(gameTurns) {
   return currentPlayer;
 }
 
-function App() {
-  const [players, setPlayers] = useState({
-    X: 'Player 1',
-    O: 'Player 2',
-  });
-  const [gameTurns, setGameTurns] = useState([]);
-  // const [hasWinners, setHasWinners] = useState(false);
-
-  const activePlayer = deriveActivePlayer(gameTurns);
-
-  let gameBoard = [...initialGameBoard.map((innerArray) => [...innerArray])];
-
-  for (const turn of gameTurns) {
-    const { square, player } = turn;
-    const { row, col } = square;
-
-    gameBoard[row][col] = player;
-  }
-
+function deriveWinner(gameBoard, players) {
   let winner;
+
   for (const combination of WINNING_COMBINATIONS) {
     const firstSquareSymbol =
       gameBoard[combination[0].row][combination[0].column];
@@ -58,7 +45,28 @@ function App() {
       winner = players[firstSquareSymbol];
     }
   }
+  return winner;
+}
 
+function deriveGameBoard(gameTurns) {
+  let gameBoard = [...INITIAL_GAME_BOARD.map((innerArray) => [...innerArray])];
+
+  for (const turn of gameTurns) {
+    const { square, player } = turn;
+    const { row, col } = square;
+
+    gameBoard[row][col] = player;
+  }
+  return gameBoard;
+}
+
+function App() {
+  const [players, setPlayers] = useState(PLAYERS);
+  const [gameTurns, setGameTurns] = useState([]);
+
+  const activePlayer = deriveActivePlayer(gameTurns);
+  const gameBoard = deriveGameBoard(gameTurns);
+  const winner = deriveWinner(gameBoard, players);
   const hasDraw = gameTurns.length === 9 && !winner;
 
   function handleSelectSquare(rowIndex, colIndex) {
@@ -93,13 +101,13 @@ function App() {
         {/* PLAYERS */}
         <ol id="players" className="highlight-player">
           <Player
-            initialName="Player 1"
+            initialName={PLAYERS.X}
             symbol="X"
             isActive={activePlayer === 'X'}
             onChangeName={handlePlayerNameChange}
           />
           <Player
-            initialName="Player 2"
+            initialName={PLAYERS.O}
             symbol="O"
             isActive={activePlayer === 'O'}
             onChangeName={handlePlayerNameChange}
